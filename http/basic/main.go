65d860e8d56bfd205b7c842cc1345cb466838af3
huyongfeng <huyf@anpro-tech.com>
2022-04-14 15:31:29 +0800

    hyf

--- http/basic/main.go
+++ http/basic/main.go
@@ -8,9 +8,40 @@ import (
 
 func main() {
 	get()
+	post()
 }
 func get() {
 	r, err := http.Get("http://httpbin.org/get")
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(content))
+}
+
+func post() {
+	if r, err := http.Post("http://httpbin.org/post", "", nil); err != nil {
+		panic(err)
+	} else {
+		defer r.Body.Close()
+		if content, err := ioutil.ReadAll(r.Body); err != nil {
+			panic(err)
+		} else {
+			fmt.Println(string(content))
+		}
+	}
+}
+
+func put() {
+	request, err := http.NewRequest(http.MethodPut, "http://httpbin.org/put", nil)
+	if err != nil {
+		panic(err)
+	}
+	r, err := http.DefaultClient.Do(request)
 	if err != nil {
 		panic(err)
 	}
