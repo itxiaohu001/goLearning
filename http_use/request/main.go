f2ed55a37674d8dd95f5ed2dfb55a0fd29fba8e9
hyf <578209081@qq.com>
2022-04-14 16:22:11 +0800

    Initial commit

--- http_use/request/main.go
+++ http_use/request/main.go
@@ -8,7 +8,10 @@ import (
 )
 
 func main() {
+	// 如何设置查询参数参数
 	requestByParams()
+	//如何定制请求头
+	requestByHead()
 }
 func printBody(resp *http.Response) {
 	content, err := ioutil.ReadAll(resp.Body)
@@ -22,13 +25,11 @@ func requestByParams() {
 	if err != nil {
 		panic(err)
 	}
-
+	// here
 	params := make(url.Values)
 	params.Add("name", "hyf")
 	params.Add("add", "18")
-
 	request.URL.RawQuery = params.Encode()
-
 	r, err := http.DefaultClient.Do(request)
 	if err != nil {
 		panic(err)
@@ -40,4 +41,28 @@ func requestByParams() {
 		}
 	}()
 	printBody(r)
+}
+
+func requestByHead() {
+	request, err := http.NewRequest(http.MethodGet, "http://httpbin.org/get", nil)
+	if err != nil {
+		panic(err)
+	}
+	// here
+	request.Header.Add("user-agent", "chrome")
+	r, err := http.DefaultClient.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err := r.Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(content))
 }
